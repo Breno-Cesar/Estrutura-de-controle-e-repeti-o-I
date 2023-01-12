40cea7172a16dcd6cde48787e39abaac54629a01
Breno-Cesar <120606444+Breno-Cesar@users.noreply.github.com>
2023-01-11 21:07:28 -0400

    20222220002

---
+++ 20222220002.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main()
+{
+    int n, soma=0;
+    for (int i = 1; i <=50; i++)
+    {
+        printf("Digite um numero: ");
+        scanf("%d", &n);
+        if (n%2!=0)
+        {
+            soma+= n;
+        }
+        
+    }
+   printf("A soma dos numeros impares e igual a %d", soma); 
+
+}
